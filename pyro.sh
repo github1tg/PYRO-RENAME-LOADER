dce2ea01aa309a79fa7131f42e9e160215607ba8
github1tg <88201236+github1tg@users.noreply.github.com>
2022-06-16 10:36:51 +0530

    Update pyro.sh

--- pyro.sh
+++ pyro.sh
@@ -862,7 +862,7 @@ python3 bot.py   #Copyright (C) 2007 Free Software Foundation, Inc.
 #of this license document, but changing it is not allowed.
 #Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
 #Everyone is permitted to copy and distribute verbatim copies
-#of this license document, but changing it is not allowed.bh
+#of this license document, but changing it is not allowed.
 
 
 
